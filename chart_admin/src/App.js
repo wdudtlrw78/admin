012bca9167e18141c8de080680e7d363c8feef14
momo <wdudtlrw78@gmail.com>
2021-10-10 00:04:50 +0900

    feat: topbar, sidebar, featiredinfo components

--- chart_admin/src/App.js
+++ chart_admin/src/App.js
@@ -1,5 +1,18 @@
+import Sidebar from './components/Sidebar';
+import Topbar from './components/Topbar';
+import './app.css';
+import Home from './pages/home';
+
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Home />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ chart_admin/src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FeaturedInfo from '../../components/FeaturedInfo';
+import './index.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <FeaturedInfo />
+    </div>
+  );
+}
+
+export default Home;
