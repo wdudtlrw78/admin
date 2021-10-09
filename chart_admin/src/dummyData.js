c7cec352e4edb84ca0c77f6e4130137dff2d9f96
momo <wdudtlrw78@gmail.com>
2021-10-10 01:55:47 +0900

    feat: chart, widgets components

---
+++ chart_admin/src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: 'Jan',
+    'Active User': 4000,
+  },
+  {
+    name: 'Feb',
+    'Active User': 3000,
+  },
+  {
+    name: 'Mar',
+    'Active User': 5000,
+  },
+  {
+    name: 'Apr',
+    'Active User': 4000,
+  },
+  {
+    name: 'May',
+    'Active User': 2500,
+  },
+  {
+    name: 'Jan',
+    'Active User': 1500,
+  },
+  {
+    name: 'Jul',
+    'Active User': 3000,
+  },
+  {
+    name: 'Agu',
+    'Active User': 5000,
+  },
+  {
+    name: 'Sep',
+    'Active User': 4000,
+  },
+  {
+    name: 'Oct',
+    'Active User': 7000,
+  },
+  {
+    name: 'Nov',
+    'Active User': 7500,
+  },
+  {
+    name: 'Dsc',
+    'Active User': 8000,
+  },
+];
--- chart_admin/src/pages/home/index.jsx
+++ chart_admin/src/pages/home/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import Chart from '../../components/Chart';
 import FeaturedInfo from '../../components/FeaturedInfo';
 import './index.css';
+import { userData } from '../../dummyData';
+import WidgetSm from '../../components/WidgetSm';
+import WidgetLg from '../../components/WidgetLg';
 
 function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
   );
 }
